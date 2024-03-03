d92271e2bd813c6430f2f36b144f3cdc875929bc
Abdul Wahab <abdulwahab@gmail.com>
2024-03-03 12:28:47 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const http = require("http");
+const express = require("express");
+const path = require("path");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+//socket.io
+io.on("connection", (socket) => {
+  socket.on("user-message", (message) => {
+    io.emit("message", message);
+  });
+});
+
+app.use(express.static(path.resolve("./public")));
+
+app.get("/", (req, res) => {
+  return res.sendFile("/public/index.html");
+});
+const port = 9000;
+server.listen(port, () => console.log(`Server started at ${port}`));
